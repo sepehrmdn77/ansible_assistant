70c6c9f73285d182cb976657e68923cac56b5853
sepehrmdn77 <sepehrmaadani98@gmail.com>
2025-04-01 12:36:37 +0300

    ssh_hosts format fix with autopep8

--- src/ssh_hosts.py
+++ src/ssh_hosts.py
@@ -1,17 +1,20 @@
 import subprocess
-y = subprocess.check_output('whoami',shell=True,text=True)
+y = subprocess.check_output('whoami', shell=True, text=True)
 # print(y[:-1]) # printing current user
-x = open(f'/home/{y[:-1]}/.ssh/config','r')
+x = open(f'/home/{y[:-1]}/.ssh/config', 'r')
 z = x.readlines()
 # for line in z:
 #     print(line) # printing config file
-a = [line[:-1].replace('  ','') for line in z] # extracting characters in config file
-b = [char for char in [c.split() for c in a]] # spliting characters
+# extracting characters in config file
+a = [line[:-1].replace('  ', '') for line in z]
+b = [char for char in [c.split() for c in a]]  # spliting characters
 # now b is a list[list[str]]
 # the main ssh command : ssh -i ~/.ssh/id_rsa git@github.com
 # we have to pipe it to ansible so we need the path for ansible.cfg below
 # [defaults]
 # private_key_file = ~/.ssh/id_rsa
+
+
 def clear(item):
     result = []
     for items in item:
@@ -21,11 +24,13 @@ def clear(item):
             result.extend(clear(items))
     return result
 
+
 flatten_list = clear(b)
 # print(flatten_list)
 # 8 items exist for each host and for each host we need items
 # with index : 3(hostname), 5(sshuser), 7(ssh_key)
 # test = [flatten_list[5],flatten_list[3],flatten_list[7]]
+
 
 def match_host(list):
     result = []
@@ -42,12 +47,15 @@ def match_host(list):
         elif items == 'HostName':
             current_host['hostname'] = list[index + 1]
         # elif items == 'Port': # uncomment here if port needed
-        #     current_host['Port'] = list[index + 1] # uncomment here if port needed
-    
+        # current_host['Port'] = list[index + 1] # uncomment here if port
+        # needed
+
     if current_host:
         result.append(current_host)
-    
+
     return result
 
-hosts_dicts_list = match_host(flatten_list) # we have a list of dictionaries that show the hosts
+
+# we have a list of dictionaries that show the hosts
+hosts_dicts_list = match_host(flatten_list)
 x.close()
