291146c7c1dec6e92d9c33a2e371c5893bf00b66
sepehrmdn77 <sepehrmaadani98@gmail.com>
2025-04-01 13:23:42 +0300

    test_1 corrected

--- tests/test_1.py
+++ tests/test_1.py
@@ -1,15 +1,17 @@
 import pytest
-from flet import Page
+import flet as ft
+import sys
+sys.path.append('../src') 
 import main
 
 def test_app_initialization():
-    page = Page()
+    page = ft.Page()
     main.main(page)
     assert len(page.controls) > 0
 
 @pytest.mark.parametrize("label_text", ["Start", "Stop", "Reset"])
 def test_button_labels(label_text):
-    page = Page()
+    page = ft.Page()
     main.main(page)
     assert any(
         control for control in page.controls if control.label == label_text
